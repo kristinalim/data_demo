c992665cf284c76b7ed8ec61477cfb264c4a0ea1
Kristina Lim <kristinalim.ph@gmail.com>
2017-01-06 01:36:57 +0800

    Add helper method in the controllers for sending data as PDF attachment.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+
+  def send_pdf(report, filename)
+    send_data report.render, type: 'application/pdf', filename: filename, disposition: 'attachment'
+  end
 end
