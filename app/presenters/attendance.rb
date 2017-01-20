9ed9cf6a0d7e67cf060c6a41f755ca517f8c74ac
Kristina Lim <kristinalim.ph@gmail.com>
2017-01-20 18:49:24 +0800

    Add footer to multiple attendance letter PDF.

--- app/presenters/attendance.rb
+++ app/presenters/attendance.rb
@@ -2,4 +2,8 @@ class Attendance
   include ActiveModel::Model
 
   attr_accessor :student_id, :first_name, :last_name, :school_name, :school_principal_name, :absences_count
+
+  def full_name
+    [first_name, last_name].compact.join(' ').to_s
+  end
 end
--- app/reports/pdfs/attendance_letters.rb
+++ app/reports/pdfs/attendance_letters.rb
@@ -18,9 +18,17 @@ module Pdfs
             attendance_summary: attendance_summary,
             letter_date: Time.zone.today,
             school_phone: '+0123456789' # TODO: Replace with actual school phone number
-          },
-          layout: 'application'
-        })
+          }
+        }),
+        layout: 'application',
+        footer: {
+          content: context.render_to_string('pdfs/attendance_letters/footer', {
+            locals: {
+              attendances: attendances
+            },
+            layout: false
+          })
+        }
       )
     end
   end
